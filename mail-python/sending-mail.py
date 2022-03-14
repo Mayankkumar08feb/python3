f6f334ed4f8b63ab1bfd7bbb83a566057f239033
Mayank Kumar <43399389+Mayankkumar08feb@users.noreply.github.com>
2022-03-15 01:06:28 +0530

    Update sending-mail.py

--- mail-python/sending-mail.py
+++ mail-python/sending-mail.py
@@ -4,7 +4,7 @@ Sending mail through python
 import smtplib
 server = smtplib.SMTP('smtp.gmail.com',587)
 server.starttls()
-server.login ("nagiosmail3","Nagios@12345")
+server.login ("nagiosmail3","Nagios@123456789")
 msg = "This is python program!!"
 server.sendmail("nagiosmail@gmail.com","nagiosmail@gmail.com", msg)
 server.quit()
