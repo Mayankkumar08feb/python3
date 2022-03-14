9af8d409e90b50dfea6f9e0d464ea9baf732e60d
Mayank Kumar <43399389+Mayankkumar08feb@users.noreply.github.com>
2022-03-15 01:07:12 +0530

    Update mymailmodule.py

--- mail-python/mymailmodule.py
+++ mail-python/mymailmodule.py
@@ -5,7 +5,7 @@ import smtplib
 def my_mail_mod(msg_to_send):
     server = smtplib.SMTP('smtp.gmail.com',587)
     server.starttls()
-    server.login ("nagiosmail3","Nagios@12345")
+    server.login ("nagiosmail3","Nagios@123456789")
     msg = "This is python program!!, sending 2nd msg"
     server.sendmail("nagiosmail3@gmail.com","nagiosmail3@gmail.com", msg)
     server.quit()
